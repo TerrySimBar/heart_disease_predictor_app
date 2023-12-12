ac30af42c23dfc9153feb75e11652d7c4e55334a
Terry Ann Barnett <terryann.barnett@gmail.com>
2023-12-11 20:07:26 -0500

    upload draft

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# app.py
-
 from flask import Flask, request, render_template
 import joblib
 import numpy as np
@@ -7,21 +5,20 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the logistic regression model
-model_path = 'random_forest_model.joblib'
+model_path = 'model/random_forest_model.joblib'
 loaded_model = joblib.load(model_path)
 
+# Define a function to preprocess the input data
 def preprocess_input(data):
-    # You might need to preprocess the input data based on how your model was trained
-    # For example, convert strings to numeric values, scale features, etc.
-    # Here, I assume all inputs are numeric, but you might need additional preprocessing.
    return np.array([float(data[key]) for key in data.keys()]).reshape(1, -1)
 
 
-
+# Define the home page route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Define the prediction route
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.form.to_dict()
@@ -32,7 +29,7 @@ def predict():
     # Make predictions using the loaded model
     predictions = loaded_model.predict(input_data)
 
-    # Assuming a binary classifier, you can return the result
+    # Render the `result.html` template with the prediction results
     return render_template('result.html', prediction=int(predictions[0]))
 
 if __name__ == '__main__':
